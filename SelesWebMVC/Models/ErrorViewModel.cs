fccbf7e6bef44d78fbd2bd8a94aa1c20c53c751e
Alberto Cavalcanti <cavalcanti_alberto@yahoo.com.br>
2020-02-21 16:46:32 -0300

    Project created

---
+++ SelesWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SelesWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
